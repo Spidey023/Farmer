8fdcf3b7d82ed1f554e1f267b140bfc145d50df1
Spidey0323 <bharathn129@gmail.com>
2025-10-08 15:56:36 -0400

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from 'dotenv'
+import app from "./app"
+
+dotenv.config({
+    path:'../.env'
+})
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log('server is running in', port)
+})
